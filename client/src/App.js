4da08aaed18bf891424d1e0b09f3ce28c153aa49
adam <bcuz@users.noreply.github.com>
2019-06-15 13:32:53 -0400

    login react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Route, NavLink, BrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+// import Register from "./components/Register";
+// import PrivateRoute from "./components/PrivateRoute";
+// import UsersList from "./components/UsersList";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi      
-    </div>
+      <BrowserRouter>
+        <div>
+        {/* <button onClick={this.logout}>Log Out</button> */}
+        <ul className='nav'>
+          <li>
+            <NavLink to="/register">Register</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/users">Users</NavLink>
+          </li>
+        </ul>
+        {/* <Route path="/register" component={Register} /> */}
+        <Route path="/login" component={Login} />
+        {/* <PrivateRoute exact path="/users" component={UsersList} /> */}
+        </div>
+      </BrowserRouter>
   );
 }
 
