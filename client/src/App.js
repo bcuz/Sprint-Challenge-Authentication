2f66fac213ea17be969cb645fb8529d9c5884f71
adam <bcuz@users.noreply.github.com>
2019-06-15 13:46:56 -0400

    logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Route, NavLink, BrowserRouter } from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import PrivateRoute from "./components/PrivateRoute";
 import JokesList from "./components/JokesList";
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  logout = () => {
+    localStorage.removeItem("token");
+    this.props.history.push("/login")
+  }
+
+  render() {
+ 
   return (
-      <BrowserRouter>
         <div>
-        {/* <button onClick={this.logout}>Log Out</button> */}
+        <button onClick={this.logout}>Log Out</button>
         <ul className='nav'>
           <li>
             <NavLink to="/register">Register</NavLink>
@@ -26,8 +33,8 @@ function App() {
         <Route path="/login" component={Login} />
         <PrivateRoute exact path="/api/jokes" component={JokesList} />
         </div>
-      </BrowserRouter>
   );
+  }
 }
 
-export default App;
+export default withRouter(App);
